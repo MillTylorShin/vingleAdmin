a3fc183716a523bd3632d8fc37beebda83b4c55d
Tylor Shin <tylor.shin@vingle.net>
2016-01-18 13:18:13 +0900

    WIP to setting up

--- client.js
+++ client.js
@@ -1,1 +1,18 @@
-console.log("Hello Webpack! Hello Babel!!");
+import React            from 'react';
+import Router           from 'react-router';
+import BrowserHistory   from 'react-router';
+import polyfill         from 'babel-polyfill';
+
+import routes           from './app/routes/routes';
+
+export default (() => {
+
+ // Creating history object for the client router
+ const history = new BrowserHistory();
+
+ // Flushing application to DOM
+ React.render(
+   <Router history={history} children={routes} />,
+   document.getElementById('pengyou')
+ );
+})();
